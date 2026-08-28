6e79a3a4c719868e63822a3fff6e38a3b8974a61
Ankur Sheel <ankur.sheel@gmail.com>
2026-08-28 23:43:20 +1200

    update accountDTO return isClosed property

--- src/NetWorthTracker.Models/Features/Account/AccountDTO.cs
+++ src/NetWorthTracker.Models/Features/Account/AccountDTO.cs
@@ -1,7 +1,25 @@
 ﻿namespace NetWorthTracker.Core.Features.Account;
 
-public record AccountDTO(
-    int Id,
-    string Name,
-    AccountType Type,
-    DateTime? ClosedDate);
+public record AccountDTO
+{
+    public AccountDTO(int Id,
+        string Name,
+        AccountType Type,
+        DateTime? ClosedDate)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.Type = Type;
+        this.ClosedDate = ClosedDate;
+    }
+
+    public int Id { get; init; }
+
+    public string Name { get; init; }
+
+    public AccountType Type { get; init; }
+
+    public DateTime? ClosedDate { get; init; }
+    
+    public bool IsClosed => ClosedDate.HasValue;
+}
